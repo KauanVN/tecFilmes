20c8e8360fcc9dad041fee664358e028992298c3
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-04-11 12:06:44 -0300

    Add files via upload

---
+++ src/data/movies.js
@@ -0,0 +1,68 @@
+const DATA =
+[
+    {
+  id: '01',
+  nome: 'Adão Negro',
+  nota:'8.8',
+  imagem: '1.jpg'
+    },
+
+    {
+        id: '02',
+        nome: 'Gatos de Botas 2',
+        nota:'8.8',
+        imagem: '2.jpg'
+   },
+   {
+        id: '03',
+        nome: 'Avatar',
+        nota:'9.0',
+        imagem: '3.jpg'
+    },
+
+    {
+        id: '04',
+        nome: 'Pantera Negra',
+        nota:'7.0',
+        imagem: '4.jpg'        
+    },
+    {
+        id: '05',
+        nome: 'CREED III',
+        nota:'8.8',
+        imagem: '5.jpg'            
+    },
+    {
+        id: '06',
+        nome: 'Pânico 6',
+        nota:'8.8',
+        imagem: '6.jpg'
+        
+    },
+    {
+        id: '07',
+        nome: 'Segundo Tempo',
+        nota:'8.8',
+        imagem: '7.jpg'
+    },
+    {
+        id: '08',
+        nome: 'O exorcista do Papa',
+        nota:'8.8',
+        imagem: '8.jpg'
+    },
+    {
+        id: '09',
+        nome: 'O urso do pó branco',
+        nota:'7.8',
+        imagem: '9.jpg'
+    },
+    {
+        id: '10',
+        nome: 'A baleia',
+        nota:'9.5',
+        imagem: '10.jpg'
+    }
+            
+
+]
