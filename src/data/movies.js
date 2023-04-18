5f27c01ba0489f3d10cf9da33bcc2512c3050630
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-04-18 11:06:04 -0300

    Add files via upload

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,68 +1,63 @@
 const DATA =
 [
     {
-  id: '01',
-  nome: 'Adão Negro',
-  nota:'8.8',
-  imagem: '1.jpg'
+        id: '01',
+        nome: 'Adão Negro',
+        nota: '8.8',
+        imagem: '1.jpg'
     },
-
     {
         id: '02',
-        nome: 'Gatos de Botas 2',
-        nota:'8.8',
-        imagem: '2.jpg'
-   },
-   {
-        id: '03',
         nome: 'Avatar',
-        nota:'9.0',
-        imagem: '3.jpg'
+        nota: '8.3',
+        imagem: '2.jpg'    
     },
-
+    {
+        id: '03',
+        nome: 'Gato de Botas 2',
+        nota: '9.7',
+        imagem: '3.jpg'    
+    },
     {
         id: '04',
         nome: 'Pantera Negra',
-        nota:'7.0',
-        imagem: '4.jpg'        
+        nota: '8.2',
+        imagem: '4.jpg'    
     },
     {
         id: '05',
-        nome: 'CREED III',
-        nota:'8.8',
-        imagem: '5.jpg'            
+        nome: 'Homem-Aranha: Sem Volta para Casa',
+        nota: '8.4',
+        imagem: '5.jpg'    
     },
     {
         id: '06',
-        nome: 'Pânico 6',
-        nota:'8.8',
-        imagem: '6.jpg'
-        
+        nome: 'Super Mario Bros',
+        nota: '8.0',
+        imagem: '6.jpg'    
     },
     {
         id: '07',
-        nome: 'Segundo Tempo',
-        nota:'8.8',
-        imagem: '7.jpg'
+        nome: 'One Piece Red',
+        nota: '9.6',
+        imagem: '7.jpg'    
     },
     {
         id: '08',
-        nome: 'O exorcista do Papa',
-        nota:'8.8',
-        imagem: '8.jpg'
+        nome: 'Doutor Estranho no Multiverso da Loucura',
+        nota: '9.0',
+        imagem: '8.jpg'    
     },
     {
         id: '09',
-        nome: 'O urso do pó branco',
-        nota:'7.8',
-        imagem: '9.jpg'
+        nome: 'Harry Potter',
+        nota: '9.8',
+        imagem: '9.jpg'    
     },
     {
         id: '10',
-        nome: 'A baleia',
-        nota:'9.5',
-        imagem: '10.jpg'
+        nome: 'Shazam! Fúria dos Deuses',
+        nota: '7.0',
+        imagem: '10.jpg'    
     }
-            
-
 ]
